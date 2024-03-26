87d9f61e5a9be139df3d2bb167f9284e8f300d4b
KVirB <dlaydrygaakk@mail.ru>
2024-03-26 16:51:41 +0300

    fix: delete  garbage

--- src/store/subject/services.ts
+++ src/store/subject/services.ts
@@ -1,13 +1,6 @@
 import { DEVELOPMENT, fetchConfig } from 'config/constants';
 import instance from 'store/auth/instance';
 
-const token = JSON.parse(localStorage.getItem('user')).access_token;
-const fetchConfig = {
-    headers: {
-        Authorization: `Bearer ${token ? token : ''}`,
-    },
-};
-
 export const fetchSubjects = async () => {
     const { data: subjects } = await instance.get(`${DEVELOPMENT}/subject`, fetchConfig);
     return subjects;
